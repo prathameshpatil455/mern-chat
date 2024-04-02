1bfca52bf01c29228e9a3525ecbf29424f305766
prathameshpatil455 <132359689+prathameshpatil455@users.noreply.github.com>
2024-04-02 11:19:47 +0530

    displaying the messages

--- api/index.js
+++ api/index.js
@@ -25,11 +25,38 @@ app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
-}))
+}));
+
+async function getUserDataFromRequest(req) {
+  return new Promise((resolve, reject) => {
+    const token = req.cookies?.token;
+    if (token) {
+      jwt.verify(token, jwtSecret, {}, (err, userData) => {
+        if (err) throw err;
+        resolve(userData);
+      });
+    } else {
+      reject('no token');
+    }
+  });
+
+}
 
 
 app.get('/test', (req, res) => {
     res.json('test ok');
+});
+
+
+app.get('/messages/:userId', async (req,res) => {
+  const {userId} = req.params;
+  const userData = await getUserDataFromRequest(req);
+  const ourUserId = userData.userId;
+  const messages = await Message.find({
+    sender:{$in:[userId,ourUserId]},
+    recipient:{$in:[userId,ourUserId]},
+  }).sort({createdAt: 1});
+  res.json(messages);
 });
 
 
@@ -122,7 +149,7 @@ wss.on("connection", (connection, req) => {
         text,
         sender:connection.userId,
         recipient,
-        id:messageDoc._id,
+        _id:messageDoc._id,
       })));
      }
   });
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,27 +1,38 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Avatar from "./Avatar";
 import Logo from "./Logo";
 import { UserContext } from "./UserContext";
-import {uniqBy} from "loadash";
+import { uniqBy } from "lodash";
+import axios from "axios";
 
 export default function Chat() {
-
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
   const [selectedUserId, setSelectedUserId] = useState(null);
-  const {username, id} = useContext(UserContext);
+  const { username, id } = useContext(UserContext);
   const [newMessageText, setNewMessageText] = useState("");
   const [messages, setMessages] = useState([]);
+  const divUnderMessages = useRef();
 
   useEffect(() => {
+    connectToWs();
+  }, [selectedUserId]);
+
+  function connectToWs() {
     const ws = new WebSocket('ws://localhost:4040');
     setWs(ws);
-    ws.addEventListener('message', handleMessage )
-  }, []);
+    ws.addEventListener('message', handleMessage);
+    ws.addEventListener('close', () => {
+      setTimeout(() => {
+        console.log('Disconnected. Trying to reconnect.');
+        connectToWs();
+      }, 1000);
+    });
+  }
 
   function showOnlinePeople(peopleArray) {
     const people = {};
-    peopleArray.forEach(({userId, username}) => {
+    peopleArray.forEach(({ userId, username }) => {
       people[userId] = username;
     });
     setOnlinePeople(people);
@@ -29,107 +40,140 @@ export default function Chat() {
 
   function handleMessage(event) {
     const messageData = JSON.parse(event.data);
-    console.log({event, messageData});
-    if ('online' in messageData) {
+    console.log({ event, messageData });
+    if ("online" in messageData) {
       showOnlinePeople(messageData.online);
-    }
-    else if ('text' in messageData) {
-      setMessages(prev => ([...prev, {...messageData}]));
+    } else if ("text" in messageData) {
+      setMessages((prev) => [...prev, { ...messageData }]);
     }
   }
 
   function sendMessage(ev) {
     ev.preventDefault();
-    ws.send(JSON.stringify({
+    ws.send(
+      JSON.stringify({
         recipient: selectedUserId,
         text: newMessageText,
-    }));
+      })
+    );
     setNewMessageText("");
-    setMessages(prev => ([...prev, {
-      text: newMessageText,
-      sender: id, 
-      recipient: selectedUserId, 
-    }]));
+    setMessages((prev) => ([
+      ...prev,
+      {
+        text: newMessageText,
+        sender: id,
+        recipient: selectedUserId,
+        _id: Date.now(),
+      },
+    ]));
   }
 
-  const onlinePeopleExclOurUser = {...onlinePeople};
+  useEffect(() => {
+    const div = divUnderMessages.current;
+    if (div) {
+      div.scrollIntoView({behavior:'smooth', block:'end'});
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (selectedUserId) {
+      axios.get('/messages/'+selectedUserId).then(res => {
+        setMessages(res.data);
+      });
+    }
+  }, [selectedUserId]);
+
+  const onlinePeopleExclOurUser = { ...onlinePeople };
   delete onlinePeopleExclOurUser[id];
 
-  const messagesWithoutDupes = uniqBy(messages, '_id');
+  const messagesWithoutDupes = uniqBy(messages, "_id");
 
   return (
     <div className="flex h-screen">
       <div className="bg-white w-1/3">
         <Logo />
-        {Object.keys(onlinePeopleExclOurUser).map(userId => (
-          <div className={"border-b border-gray-100  flex items-center gap-2 cursor-pointer"+ (userId === selectedUserId ? "bg-blue-200" : '')}
-           key={userId}
-           onClick={() => setSelectedUserId(userId)}>
-            {
-              userId === selectedUserId && (
-                <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
-              )
+        {Object.keys(onlinePeopleExclOurUser).map((userId) => (
+          <div
+            className={
+              "border-b border-gray-100  flex items-center gap-2 cursor-pointer" +
+              (userId === selectedUserId ? "bg-blue-200" : "")
             }
+            key={userId}
+            onClick={() => setSelectedUserId(userId)}
+          >
+            {userId === selectedUserId && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
             <div className="flex gap-2 py-2 pl-4 items-center">
               <Avatar username={onlinePeople[userId]} userId={userId} />
               <span className="text-gray-800">{onlinePeople[userId]}</span>
             </div>
-            
           </div>
         ))}
       </div>
 
       <div className="flex flex-col bg-blue-50 w-2/3 p-2">
         <div className="flex-grow">
-          {
-            !selectedUserId && (
-              <div className="flex h-full items-center justify-center">
-                <div className="text-gray-400 ">
-                  &larr; Select a person 
-                </div>
-              </div>
-            )
-          }
-          {
-            !!selectedUserId && (
-              <div>
-                {messagesWithoutDupes.map(message => (
-                  <div>
-                    {message.sender === id ? 'ME' : ''}
-                    {message.text}
+          {!selectedUserId && (
+            <div className="flex h-full items-center justify-center">
+              <div className="text-gray-400 ">&larr; Select a person</div>
+            </div>
+          )}
+          {!!selectedUserId && (
+            <div className="relative h-full">
+              <div className="overflow-y-scroll absolute top=0 left-0 right-0 bottom-2">
+                {messagesWithoutDupes.map((message) => (
+                  <div
+                    className={
+                      message.sender === id ? "text-right" : "text-left"
+                    } key={message._id}
+                  >
+                    <div
+                      className={
+                        "text-left inline-block p-2 my-2 rounded-sm text-sm" +
+                        (message.sender === id
+                          ? "bg-blue-500 text-white"
+                          : "bg-white text-gray-500")
+                      }
+                    >
+                      {message.text}
+                    </div>
                   </div>
                 ))}
+                <div ref={divUnderMessages}></div>
               </div>
-            )
-          }
+            </div>
+          )}
         </div>
 
         {!!selectedUserId && (
           <form className="flex gap-2" onSubmit={sendMessage}>
-          <input
-            value={newMessageText}
-            onChange={ev => setNewMessageText(ev.target.value)}
-            type="text"
-            placeholder="type your message here"
-            className="bg-white flex-grow border p-2 rounded-sm "
-          />
-          <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-4 h-4"
+            <input
+              value={newMessageText}
+              onChange={(ev) => setNewMessageText(ev.target.value)}
+              type="text"
+              placeholder="type your message here"
+              className="bg-white flex-grow border p-2 rounded-sm "
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 p-2 text-white rounded-sm"
             >
-              <path
-                fillRule="evenodd"
-                d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </form>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </form>
         )}
-        
       </div>
     </div>
   );
